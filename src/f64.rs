7c4cd6979ded7ded0140f5f1cdd636269766c26d
Brian Anderson <andersrb@gmail.com>
2022-03-23 01:34:07 -0600

    .

--- src/f64.rs
+++ src/f64.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::path::{Path, PathBuf};
-use crate::math::{Snat32, Zone64};
+use crate::math::{Snat32, Zone64, One64};
 
 fn line_y_value(
     y_rise: f64,
@@ -28,6 +28,14 @@ const A440_SAMPLES: i32 = SAMPLE_RATE_KHZ / 440;
 struct Oscillator {
     period: Snat32,
     phase: i32,
-    rise_time: i32, // 0 for sawtooth, period / 2 for triangle
+    rise_time: Snat32, // 0 for sawtooth, period / 2 for triangle
     squareness: Zone64, // 0 for saw/tri, 1 for square
+    pulse_width: Snat32,
+}
+
+impl Oscillator {
+    fn sample(&self, offset: Snat32) -> One64 {
+        //assert!(self.rise_time <= self.period / Snat32::from(2));
+        todo!()
+    }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use std::ops::Div;
+use std::cmp::{Ord, PartialOrd, Eq, PartialEq, Ordering};
 
 /// Positive i32 (signed natural)
 pub struct Snat32(i32);
@@ -24,6 +26,35 @@ impl TryFrom<i32> for Snat32 {
     }
 }
 
+impl Div for Snat32 {
+    type Output = Snat32;
+
+    fn div(self, other: Snat32) -> Snat32 {
+        Snat32(self.0 / other.0)
+    }
+}
+
+impl PartialEq for Snat32 {
+    fn eq(&self, other: &Snat32) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl Eq for Snat32 { }
+
+impl PartialOrd for Snat32 {
+    fn partial_cmp(&self, other: &Snat32) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Ord for Snat32 {
+    fn cmp(&self, other: &Snat32) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+
 /// F64 between zero and one
 pub struct Zone64(f64);
 
@@ -44,6 +75,30 @@ impl TryFrom<f64> for Zone64 {
             Ok(Zone64(other))
         } else {
             Err(anyhow::anyhow!("float out of [0, 1] range"))
+        }
+    }
+}
+
+/// F64 between negative one and one
+pub struct One64(f64);
+
+impl One64 {
+}
+
+impl From<One64> for f64 {
+    fn from(other: One64) -> f64 {
+        other.0
+    }
+}
+
+impl TryFrom<f64> for One64 {
+    type Error = anyhow::Error;
+
+    fn try_from(other: f64) -> Result<One64> {
+        if other >= -1.0 && other <= 1.0 {
+            Ok(One64(other))
+        } else {
+            Err(anyhow::anyhow!("float out of [-1, 1] range"))
         }
     }
 }
