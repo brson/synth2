942af904026150b518a3a1631109815bc9ee1fb6
Brian Anderson <andersrb@gmail.com>
2022-03-23 01:18:43 -0600

    .

--- src/f64.rs
+++ src/f64.rs
@@ -1,6 +1,33 @@
 use anyhow::Result;
 use std::path::{Path, PathBuf};
+use crate::math::{Snat32, Zone64};
+
+fn line_y_value(
+    y_rise: f64,
+    x_run: f64,
+    x_value: f64,
+) -> f64 {
+    let slope = y_rise / x_run;
+    let y_value = slope * x_value;
+    y_value
+}
+
+fn line_y_value_with_y_offset(
+    y_rise: f64,
+    x_run: f64,
+    x_value: f64,
+    y_offset: f64,
+) -> f64 {
+    let y_value = line_y_value(y_rise, x_run, x_value);
+    y_value + y_offset
+}
 
 const SAMPLE_RATE_KHZ: i32 = 32_000;
 const A440_SAMPLES: i32 = SAMPLE_RATE_KHZ / 440;
 
+struct Oscillator {
+    period: Snat32,
+    phase: i32,
+    rise_time: i32, // 0 for sawtooth, period / 2 for triangle
+    squareness: Zone64, // 0 for saw/tri, 1 for square
+}
--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 
+/// Positive i32 (signed natural)
 pub struct Snat32(i32);
 
 impl Snat32 {
@@ -19,6 +20,30 @@ impl TryFrom<i32> for Snat32 {
             Ok(Snat32(other))
         } else {
             Err(anyhow::anyhow!("negative value"))
+        }
+    }
+}
+
+/// F64 between zero and one
+pub struct Zone64(f64);
+
+impl Zone64 {
+}
+
+impl From<Zone64> for f64 {
+    fn from(other: Zone64) -> f64 {
+        other.0
+    }
+}
+
+impl TryFrom<f64> for Zone64 {
+    type Error = anyhow::Error;
+
+    fn try_from(other: f64) -> Result<Zone64> {
+        if other >= 0.0 && other <= 1.0 {
+            Ok(Zone64(other))
+        } else {
+            Err(anyhow::anyhow!("float out of [0, 1] range"))
         }
     }
 }
