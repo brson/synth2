88ba5cd845958682b6606590b7efd5b962a23916
Brian Anderson <andersrb@gmail.com>
2022-03-31 20:49:59 -0600

    .

--- src/f64.rs
+++ src/f64.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use std::path::{Path, PathBuf};
 use crate::math::{Snat32, ZOne64, One64, AssertFrom, ZPos64};
 
@@ -303,6 +303,55 @@ fn write_image(buf: &[f64], outdir: &Path, file_stem: &str) -> Result<()> {
     Ok(())
 }
 
+fn write_image_ch(buf: &[f64], outdir: &Path, file_stem: &str) -> Result<()> {
+    use charts::{Chart, ScaleLinear, MarkerType, PointLabelPosition, LineSeriesView};
+
+    let filepath = outdir.join(file_stem).with_extension("svg");
+
+    let width = 1280;
+    let height = 720;
+    let (top, right, bottom, left) = (100, 100, 100, 100);
+
+    let available_width = width - left - right;
+    let available_height = height - top - bottom;
+
+    let x = ScaleLinear::new()
+        .set_domain(vec![0_f32, buf.len() as f32])
+        .set_range(vec![0, available_width]);
+
+    let y = ScaleLinear::new()
+        .set_domain(vec![-1_f32, 1_f32])
+        .set_range(vec![available_height, 0]);
+
+    let line_data = (0..).zip(buf.iter().copied());
+    let line_data = line_data.map(|(x, y)| (x as f32, y as f32));
+    let line_data: Vec<_> = line_data.collect();
+
+    let line_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_marker_type(MarkerType::Circle)
+    //.set_label_position(PointLabelPosition::N)
+        .set_label_visibility(false)
+        .load_data(&line_data)
+        .map_err(|e| anyhow!("{}", e))?;
+
+    Chart::new()
+        .set_width(width)
+        .set_height(height)
+        .set_margins(top, right, bottom, left)
+        //.add_title(String::from("Line Chart"))
+        .add_view(&line_view)
+        .add_axis_bottom(&x)
+        .add_axis_left(&y)
+        //.add_left_axis_label("Custom Y Axis Label")
+        //.add_bottom_axis_label("Custom X Axis Label")
+        .save(filepath)
+        .map_err(|e| anyhow!("{}", e))?;
+
+    Ok(())
+}
+
 fn fill_buf_osc(buf: &mut [f64], osc: Oscillator) {
     for i in 0..buf.len() {
         let sample = osc.sample(Snat32::assert_from(i as i32));
@@ -313,7 +362,7 @@ fn fill_buf_osc(buf: &mut [f64], osc: Oscillator) {
 fn write_test_osc(name: &str, osc: Oscillator) -> Result<()> {
     let mut buf = vec![0_f64; A440_SAMPLES as usize];
     fill_buf_osc(&mut buf, osc);
-    write_image(&buf, &PathBuf::from("out"), name)
+    write_image_ch(&buf, &PathBuf::from("out"), name)
 }
 
 pub fn run() -> Result<()> {
