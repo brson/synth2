214ad8ff8b1cf6e04a65a8f60dea2a655606dfaf
Brian Anderson <andersrb@gmail.com>
2022-04-03 16:13:38 -0600

    .

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,38 @@
 use std::sync::mpsc::{self, Sender, Receiver};
 use anyhow::Result;
-use std::thread::JoinHandle;
+use std::thread::{self, JoinHandle};
 
 pub fn run() -> Result <()> {
+    let (tx_controller, rx_controller) = mpsc::channel::<ControllerMsg>();
+    let (tx_input, rx_input) = mpsc::channel::<InputMsg>();
+    let (tx_sequencer, rx_sequencer) = mpsc::channel::<SequencerMsg>();
+    let (tx_audio_server, rx_audio_server) = mpsc::channel::<AudioServerMsg>();
+
+    let controller_thread = ControllerThread {
+        rx: rx_controller,
+        tx_input,
+        tx_sequencer,
+        tx_audio_server,
+    };
+
+    let input_thread = InputThread {
+        rx: rx_input,
+        tx_controller: tx_controller.clone(),
+    };
+
+    let sequencer_thread = SequencerThread {
+        rx: rx_sequencer,
+        tx_controller: tx_controller.clone(),
+    };
+
+    let audio_server_thread = AudioServerThread {
+        rx: rx_audio_server,
+        tx_controller: tx_controller.clone(),
+    };
+
+    drop(tx_controller);
+
+    
     todo!()
 }
 
@@ -15,22 +45,24 @@ struct Threads {
 
 struct ControllerThread {
     rx: Receiver<ControllerMsg>,
-    input_tx: Sender<InputMsg>,
+    tx_input: Sender<InputMsg>,
+    tx_sequencer: Sender<SequencerMsg>,
+    tx_audio_server: Sender<AudioServerMsg>,
 }
 
 struct InputThread {
     rx: Receiver<InputMsg>,
-    contoller_tx: Sender<ControllerMsg>,
+    tx_controller: Sender<ControllerMsg>,
 }
 
 struct SequencerThread {
     rx: Receiver<SequencerMsg>,
-    controller_tx: Sender<ControllerMsg>,
+    tx_controller: Sender<ControllerMsg>,
 }
 
 struct AudioServerThread {
     rx: Receiver<AudioServerMsg>,
-    controller_tx: Sender<ControllerMsg>,
+    tx_controller: Sender<ControllerMsg>,
 }
 
 enum ControllerMsg {
