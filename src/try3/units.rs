24a4fe1264ee53f0a177692923e1b76cf9aacb90
Brian Anderson <andersrb@gmail.com>
2022-06-11 21:16:43 -0600

    .

--- src/try3/units.rs
+++ src/try3/units.rs
@@ -15,3 +15,14 @@ pub struct SampleRateKhz(pub u32);
 
 #[derive(Copy, Clone)]
 pub struct SampleOffset(pub f32);
+
+impl Ms {
+    /// Get the time as samples
+    pub fn as_samples(&self, sample_rate: SampleRateKhz) -> f32 {
+        let sample_rate = sample_rate.0 as f32;
+        let ms = self.0;
+        let seconds = ms / 1000.0;
+        let samples = sample_rate * seconds;
+        samples
+    }
+}    
