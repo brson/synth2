107b0d5b35cebe9183a4b76d74070d329e1591b6
Brian Anderson <andersrb@gmail.com>
2022-06-21 16:08:34 -0600

    .

--- src/try3/oscillators.rs
+++ src/try3/oscillators.rs
@@ -20,7 +20,7 @@ pub struct TriangleOscillator {
 }
 
 impl Oscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Unipolar<1> {
+    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
         match self {
             Oscillator::Square(osc) => osc.sample(offset),
             Oscillator::Triangle(osc) => osc.sample(offset),
@@ -30,19 +30,68 @@ impl Oscillator {
 }
 
 impl SquareOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Unipolar<1> {
-        todo!()
+    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+        let period = self.period.0;
+        let offset = offset.0;
+        let offset = offset % period;
+
+        let half_period = period / 2.0;
+        let sample = if offset < half_period {
+            1.0
+        } else {
+            -1.0
+        };
+
+        Bipolar(sample)
     }
 }
 
 impl SawOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Unipolar<1> {
-        todo!()
+    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+        let period = self.period.0;
+        let offset = offset.0;
+        let offset = offset % period;
+
+        let x_rise = -2.0;
+        let x_run = period;
+        let x_value = offset;
+        let y_offset = 1.0;
+
+        let sample = line_y_value_with_y_offset(
+            x_rise, x_run, x_value, y_offset
+        );
+
+        Bipolar(sample)
     }
 }
 
 impl TriangleOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Unipolar<1> {
-        todo!()
+    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+        let period = self.period.0;
+        let offset = offset.0;
+        let offset = offset % period;
+
+        let half_period = period / 2.0;
+        let sample = if offset < half_period {
+            let x_rise = -2.0;
+            let x_run = half_period;
+            let x_value = offset;
+            let y_offset = 1.0;
+
+            line_y_value_with_y_offset(
+                x_rise, x_run, x_value, y_offset
+            )
+        } else {
+            let x_rise = 2.0;
+            let x_run = half_period;
+            let x_value = offset - half_period;
+            let y_offset = -1.0;
+
+            line_y_value_with_y_offset(
+                x_rise, x_run, x_value, y_offset
+            )
+        };
+
+        Bipolar(sample)
     }
 }
