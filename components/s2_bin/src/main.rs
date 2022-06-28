21a9c56313b6a888df4e5f1a449211ed66139f82
Brian Anderson <andersrb@gmail.com>
2022-06-27 21:41:00 -0600

    start adding midi

--- components/s2_bin/src/main.rs
+++ components/s2_bin/src/main.rs
@@ -19,9 +19,23 @@ fn main() -> Result<()> {
 
     match opts {
         Command::Midi => {
+            do_midi()?;
         }
     }
 
     Ok(())
 }
 
+fn do_midi() -> Result<()> {
+    use midir::{MidiInput, Ignore};
+
+    let mut midi_in = MidiInput::new("midir test input")?;
+    midi_in.ignore(Ignore::None);
+
+    println!("Available input ports:");
+    for (i, p) in midi_in.ports().iter().enumerate() {
+        println!("{}: {}", i, midi_in.port_name(p)?);
+    }
+
+    Ok(())
+}
