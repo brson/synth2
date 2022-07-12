1501a27d00627ebd78d83e1a3eca52bf22c9a637
Brian Anderson <andersrb@gmail.com>
2022-07-12 17:39:48 -0600

    .

--- components/s2_lib/src/try3/oscillators.rs
+++ components/s2_lib/src/try3/oscillators.rs
@@ -1,87 +1,109 @@
-use super::math::*;
-use super::units::*;
+pub use basic::*;
 
-pub enum Oscillator {
-    Square(SquareOscillator),
-    Saw(SawOscillator),
-    Triangle(TriangleOscillator),
-}
+pub mod basic {
+    use super::super::math::*;
+    use super::super::units::*;
 
-pub struct SquareOscillator {
-    pub period: SampleOffset,
-}
+    pub enum Oscillator {
+        Square(SquareOscillator),
+        Saw(SawOscillator),
+        Triangle(TriangleOscillator),
+    }
 
-pub struct SawOscillator {
-    pub period: SampleOffset,
-}
+    pub struct SquareOscillator {
+        pub period: SampleOffset,
+    }
 
-pub struct TriangleOscillator {
-    pub period: SampleOffset,
-}
+    pub struct SawOscillator {
+        pub period: SampleOffset,
+    }
 
-impl Oscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
-        match self {
-            Oscillator::Square(osc) => osc.sample(offset),
-            Oscillator::Triangle(osc) => osc.sample(offset),
-            Oscillator::Saw(osc) => osc.sample(offset),
+    pub struct TriangleOscillator {
+        pub period: SampleOffset,
+    }
+
+    impl Oscillator {
+        pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+            match self {
+                Oscillator::Square(osc) => osc.sample(offset),
+                Oscillator::Triangle(osc) => osc.sample(offset),
+                Oscillator::Saw(osc) => osc.sample(offset),
+            }
+        }
+    }
+
+    impl SquareOscillator {
+        pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+            let period = self.period.0;
+            let offset = offset.0;
+            let offset = offset % period;
+
+            let half_period = period / 2.0;
+            let sample = if offset < half_period { 1.0 } else { -1.0 };
+
+            Bipolar(sample)
+        }
+    }
+
+    impl SawOscillator {
+        pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+            let period = self.period.0;
+            let offset = offset.0;
+            let offset = offset % period;
+
+            let x_rise = -2.0;
+            let x_run = period;
+            let x_value = offset;
+            let y_offset = 1.0;
+
+            let sample = line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset);
+
+            Bipolar(sample)
+        }
+    }
+
+    impl TriangleOscillator {
+        pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+            let period = self.period.0;
+            let offset = offset.0;
+            let offset = offset % period;
+
+            let half_period = period / 2.0;
+            let sample = if offset < half_period {
+                let x_rise = -2.0;
+                let x_run = half_period;
+                let x_value = offset;
+                let y_offset = 1.0;
+
+                line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset)
+            } else {
+                let x_rise = 2.0;
+                let x_run = half_period;
+                let x_value = offset - half_period;
+                let y_offset = -1.0;
+
+                line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset)
+            };
+
+            Bipolar(sample)
         }
     }
 }
 
-impl SquareOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
-        let period = self.period.0;
-        let offset = offset.0;
-        let offset = offset % period;
+mod phased {
+    use super::super::units::*;
 
-        let half_period = period / 2.0;
-        let sample = if offset < half_period { 1.0 } else { -1.0 };
+    pub struct SquareOscillator {
+        pub period: SampleOffset,
+        pub phase: Unipolar<1>,
+    }
 
-        Bipolar(sample)
+    impl SquareOscillator {
+        pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
+            todo!()
+        }
     }
 }
 
-impl SawOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
-        let period = self.period.0;
-        let offset = offset.0;
-        let offset = offset % period;
-
-        let x_rise = -2.0;
-        let x_run = period;
-        let x_value = offset;
-        let y_offset = 1.0;
-
-        let sample = line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset);
-
-        Bipolar(sample)
-    }
-}
-
-impl TriangleOscillator {
-    pub fn sample(&self, offset: SampleOffset) -> Bipolar<1> {
-        let period = self.period.0;
-        let offset = offset.0;
-        let offset = offset % period;
-
-        let half_period = period / 2.0;
-        let sample = if offset < half_period {
-            let x_rise = -2.0;
-            let x_run = half_period;
-            let x_value = offset;
-            let y_offset = 1.0;
-
-            line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset)
-        } else {
-            let x_rise = 2.0;
-            let x_run = half_period;
-            let x_value = offset - half_period;
-            let y_offset = -1.0;
-
-            line_y_value_with_y_offset(x_rise, x_run, x_value, y_offset)
-        };
-
-        Bipolar(sample)
-    }
+mod phase_accumulating {
 }
