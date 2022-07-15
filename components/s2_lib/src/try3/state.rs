d50e9fcae4927a26c0d31d403c30e8c76f371851
Brian Anderson <andersrb@gmail.com>
2022-07-14 20:14:36 -0600

    more soa

--- components/s2_lib/src/try3/state.rs
+++ components/s2_lib/src/try3/state.rs
@@ -1,11 +1,20 @@
 use soa_derive::StructOfArray;
 
-pub use super::oscillators::phase_accumulating::OscillatorState;
+pub use super::oscillators::phase_accumulating::{
+    OscillatorState,
+    OscillatorStateSlice,
+    OscillatorStateSliceMut,
+    OscillatorStatePtr,
+    OscillatorStatePtrMut,
+    OscillatorStateRef,
+    OscillatorStateRefMut,
+};
 
 #[derive(StructOfArray)]
 #[derive(Default)]
 #[derive(Copy, Clone)]
 pub struct Layer {
+    #[nested_soa]
     pub osc: OscillatorState,
     #[nested_soa]
     pub lpf: LowPassFilter,
