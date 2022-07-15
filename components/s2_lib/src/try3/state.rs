ffb774bf235eeec36cd50b7fe545530b39907694
Brian Anderson <andersrb@gmail.com>
2022-07-14 20:12:07 -0600

    more soa

--- components/s2_lib/src/try3/state.rs
+++ components/s2_lib/src/try3/state.rs
@@ -2,7 +2,8 @@ use soa_derive::StructOfArray;
 
 pub use super::oscillators::phase_accumulating::OscillatorState;
 
-#[derive(StructOfArray, Default)]
+#[derive(StructOfArray)]
+#[derive(Default)]
 #[derive(Copy, Clone)]
 pub struct Layer {
     pub osc: OscillatorState,
@@ -10,7 +11,8 @@ pub struct Layer {
     pub lpf: LowPassFilter,
 }
 
-#[derive(StructOfArray, Default)]
+#[derive(StructOfArray)]
+#[derive(Default)]
 #[derive(Copy, Clone)]
 pub struct LowPassFilter {
     pub last: f32,
