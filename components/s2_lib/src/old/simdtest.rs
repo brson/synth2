3373c13f430a6542692e129fad89688098f02681
Brian Anderson <andersrb@gmail.com>
2022-08-09 14:47:13 -0600

    Add 16 lane simd oscillator

--- components/s2_lib/src/old/simdtest.rs
+++ components/s2_lib/src/old/simdtest.rs
@@ -1,14 +1,15 @@
-use std::simd::{f32x4, mask32x4, u32x4, u8x4, f32x16, u32x16};
+use std::simd::{f32x4, mask32x4, u32x4, u8x4};
+use std::simd::{f32x16, mask32x16, u32x16, u8x16};
 use std::simd::{Mask, SimdFloat, SimdPartialEq, SimdPartialOrd};
+
+pub const OSC_KIND_SQUARE: u8 = 1;
+pub const OSC_KIND_SAW: u8 = 2;
+pub const OSC_KIND_TRI: u8 = 3;
 
 pub struct OscillatorX4 {
     kind: u8x4,
     period: f32x4,
 }
-
-const OSC_KIND_SQUARE: u8 = 1;
-const OSC_KIND_SAW: u8 = 2;
-const OSC_KIND_TRI: u8 = 3;
 
 impl OscillatorX4 {
     pub fn sample(&self, offset: u32x4) -> f32x4 {
@@ -74,6 +75,83 @@ impl OscillatorX4 {
         let is_kind_tri = mask32x4::from(is_kind_tri);
 
         let sample = f32x4::splat(0.0);
+        let sample = is_kind_square.select(square_sample, sample);
+        let sample = is_kind_saw.select(saw_sample, sample);
+        let sample = is_kind_tri.select(tri_sample, sample);
+
+        sample
+    }
+}
+
+pub struct OscillatorX16 {
+    pub kind: u8x16,
+    pub period: f32x16,
+}
+
+impl OscillatorX16 {
+    pub fn sample(&self, offset: u32x16) -> f32x16 {
+        let kind = self.kind;
+        let period = self.period;
+
+        let offset = offset.to_array();
+        let offset = offset.map(|v| v as f32);
+        let offset = f32x16::from_array(offset);
+        let offset = offset % period;
+
+        let square_sample = {
+            let two = f32x16::splat(2.0);
+            let half_period = period / two;
+            if offset < half_period {
+                f32x16::splat(1.0)
+            } else {
+                f32x16::splat(-1.0)
+            }
+        };
+
+        let saw_sample = {
+            let x_rise = f32x16::splat(-2.0);
+            let x_run = period;
+            let x_value = offset;
+            let y_offset = f32x16::splat(1.0);
+
+            line_y_value_with_y_offset_x16(x_rise, x_run, x_value, y_offset)
+        };
+
+        let tri_sample = {
+            let two = f32x16::splat(2.0);
+            let half_period = period / two;
+
+            let tri_first_half_sample = {
+                let x_rise = f32x16::splat(-2.0);
+                let x_run = half_period;
+                let x_value = offset;
+                let y_offset = f32x16::splat(1.0);
+
+                line_y_value_with_y_offset_x16(x_rise, x_run, x_value, y_offset)
+            };
+            let tri_second_half_sample = {
+                let x_rise = f32x16::splat(2.0);
+                let x_run = half_period;
+                let x_value = offset - half_period;
+                let y_offset = f32x16::splat(-1.0);
+
+                line_y_value_with_y_offset_x16(x_rise, x_run, x_value, y_offset)
+            };
+
+            let in_first_half = offset.simd_lt(half_period);
+
+            in_first_half.select(tri_first_half_sample, tri_second_half_sample)
+        };
+
+        let is_kind_square = kind.simd_eq(u8x16::splat(OSC_KIND_SQUARE));
+        let is_kind_saw = kind.simd_eq(u8x16::splat(OSC_KIND_SAW));
+        let is_kind_tri = kind.simd_eq(u8x16::splat(OSC_KIND_TRI));
+
+        let is_kind_square = mask32x16::from(is_kind_square);
+        let is_kind_saw = mask32x16::from(is_kind_saw);
+        let is_kind_tri = mask32x16::from(is_kind_tri);
+
+        let sample = f32x16::splat(0.0);
         let sample = is_kind_square.select(square_sample, sample);
         let sample = is_kind_saw.select(saw_sample, sample);
         let sample = is_kind_tri.select(tri_sample, sample);
