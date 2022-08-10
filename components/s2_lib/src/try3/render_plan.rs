ab3a08a7d5a7e9c72c02cae4520c47477a762c6f
Brian Anderson <andersrb@gmail.com>
2022-08-10 16:29:56 -0600

    .

--- components/s2_lib/src/try3/render_plan.rs
+++ components/s2_lib/src/try3/render_plan.rs
@@ -2,25 +2,6 @@ use soa_derive::StructOfArray;
 
 use super::units::*;
 
-#[derive(Copy, Clone)]
-pub struct LayerX<const N: usize> {
-    pub osc: OscillatorX<N>,
-    pub lpf: LowPassFilterX<N>,
-    pub gains: [Unipolar<1>; N],
-}
-
-#[derive(Copy, Clone)]
-pub struct OscillatorX<const N: usize> {
-    pub kind: OscillatorKind,
-    pub periods: [SampleOffset; N],
-}
-
-#[derive(Copy, Clone)]
-pub struct LowPassFilterX<const N: usize> {
-    pub sample_rate: SampleRateKhz,
-    pub freqs: [Hz; N],
-}
-
 #[derive(StructOfArray)]
 #[derive(Copy, Clone)]
 pub struct Layer {
@@ -51,3 +32,23 @@ pub struct LowPassFilter {
     pub freq: Hz,
     pub sample_rate: SampleRateKhz,
 }
+
+#[derive(Copy, Clone)]
+pub struct LayerX<const N: usize> {
+    pub osc: OscillatorX<N>,
+    pub lpf: LowPassFilterX<N>,
+    pub gains: [Unipolar<1>; N],
+}
+
+#[derive(Copy, Clone)]
+pub struct OscillatorX<const N: usize> {
+    pub kind: OscillatorKind,
+    pub periods: [SampleOffset; N],
+}
+
+#[derive(Copy, Clone)]
+pub struct LowPassFilterX<const N: usize> {
+    pub sample_rate: SampleRateKhz,
+    pub freqs: [Hz; N],
+}
+
